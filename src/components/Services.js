efb36d61729ec5c4e7156867ece0e92e57aa369a
Onuorah Ike <ikecodes@gmail.com>
2022-01-19 03:03:56 +0100

    fixed services section

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Services = ({ icon, text, title }) => {
   return (
-    <div className='flex lg:justify-self-stretch  bg-white rounded px-9 py-4 items-center space-x-4'>
+    <div className='flex flex-wrap justify-center lg:justify-start justify-self-stretch  bg-white rounded px-1 sm:px-9 py-4 items-center '>
       {icon}
-      <div>
-        <p className='font-semibold'>{text}</p>
-        <p>{title}</p>
+      <div className='text-center lg:ml-4 mt-2 lg:mt-0 md:text-left'>
+        <p className='font-semibold text-xs sm:text-base whitespace-nowrap'>
+          {text}
+        </p>
+        <p className='text-xs sm:text-base'>{title}</p>
       </div>
     </div>
   );
